6cf83ea58b317a677bc2c97b6d975cea68893c31
Jihoon Seon <sjh880126@gmail.com>
2024-07-18 11:18:48 +0900

    ignore변경 및 스크립트 이름 변경

--- Programers_Level_0.cs
+++ Programers_Level_0.cs
@@ -1603,4 +1603,35 @@ public class Programers_Level_0
 
         return count;
     }
+
+
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/181880
+    public int s181880(int[] num_list)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            int value = num_list[i];
+
+            while (value != 1)
+            {
+                value /= 2;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+
+
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/181879
+    public int solution(int[] num_list)
+    {
+        int answer = 0;
+        return answer;
+    }
 }
